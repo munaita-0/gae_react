0c8d6351edcf7e20b61ce9e41ee90413a7b90023
Suzuki Shogo <shogo807@gmail.com>
2019-02-27 00:35:27 +0900

    redux base front app

--- front/src/index.js
+++ front/src/index.js
@@ -1,30 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { DatePicker, message, Alert } from 'antd';
-import 'antd/dist/antd.css';
-import './index.css';
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import Root from './containers/Root'
 
-class App extends React.Component {
-  state = {
-    date: null,
-  };
-
-  handleChange = date => {
-    message.info(`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`);
-    this.setState({date})
-  }
-
-  render() {
-    const { date } = this.state;
-    return (
-      <div style={{ width: 400, margin: '100px auto' }}>
-        <DatePicker onChange={this.handleChange} />
-        <div style={{ margingTop: 20 }}>
-          <Alert message={`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`} type='success' />
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<Root />, document.getElementById('root'))
