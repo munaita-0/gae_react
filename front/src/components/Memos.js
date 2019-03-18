8816c1cd9766a58c6f02759464d86cf12575e850
Suzuki Shogo <shogo807@gmail.com>
2019-03-18 17:37:11 +0900

    add antd

--- front/src/components/Memos.js
+++ front/src/components/Memos.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
+import { Button, Table } from 'antd'
+import 'antd/dist/antd.css'
 
 export default class Memos extends Component {
   render() {
     const { memos, handleDelete } = this.props
+
+    const dataSource = memos.map((memo, i) => ({
+      key: i.toString(),
+      id: memo.id.toString(),
+      name: memo.name,
+      description: memo.description,
+      edit: <NavLink to={`update/${memo.id}`}>Update</NavLink>,
+      delete: <Button type="primary" onClick={() => handleDelete(memo)}>delete</Button>
+    }))
+
+
+    const columns = [
+      {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: 'Description',
+        dataIndex: 'description',
+        key: 'description',
+      },
+      {
+        title: '',
+        dataIndex: 'edit',
+        key: 'edit',
+      },
+      {
+        title: '',
+        dataIndex: 'delete',
+        key: 'delete',
+      },
+    ]
+
     return (
-      <ul>
-        {memos.map((memo, i) => (
-          <li key={memo.id}>
-            {memo.id}::{memo.name}::{memo.description}
-            <NavLink to={'update/' + memo.id}>Update</NavLink>
-            <button onClick={() => handleDelete(memo)}>delete</button>
-          </li>
-        ))}
-      </ul>
+      <div>
+        <Table dataSource={dataSource} columns={columns} />
+      </div>
     )
   }
 }
