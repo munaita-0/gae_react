5325dc35079553fef9570f83e2bb4cf30d719a16
Suzuki Shogo <shogo807@gmail.com>
2019-03-29 15:36:18 +0900

    add validation

--- front/src/components/MemoForm.js
+++ front/src/components/MemoForm.js
@@ -1,24 +1,52 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
-import { Form, Button, } from 'antd'
+import { Form, Button, Input } from 'antd'
+
+const validate = values => {
+  const errors = {}
+
+  if (!values.name) {
+    errors.name = 'Required'
+  } else if (values.name.length < 2) {
+    errors.name = 'Must be more than 2 characters'
+  }
+
+  if (!values.description) {
+    errors.description = 'Required'
+  }
+
+  return errors
+}
+
+const renderField = ({ input, label, type, meta}) => {
+  const hasError = meta.touched && meta.invalid;
+
+  return (
+    <Form.Item
+      label={label}
+      validateStatus={hasError ? "error" : "success"}
+      help={hasError && meta.error}
+    >
+      <div>
+        <Input {...input} placeholder={label} type={type}/>
+      </div>
+    </Form.Item>
+  )
+}
+
 
 let MemoForm = props => {
-  const { handleSubmit } = props
+  const { handleSubmit, invalid } = props
   return <Form onSubmit={handleSubmit}>
-          <Form.Item label="Name">
-            <Field name="name" component="input" type="text" />
-          </Form.Item>
-          <Form.Item label="description">
-            <Field name="description" component="input" type="text" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" >Create</Button>
-          </Form.Item>
-        </Form>
+           <Field name="name" component={renderField} type="text" label="Name" />
+           <Field name="description" component={renderField} type="text" label="Description" />
+           <Button type="primary" htmlType="submit" disabled={invalid} >Create</Button>
+         </Form>
 }
 
 MemoForm = reduxForm({
-  form: 'memo'
+  form: 'memo',
+  validate
 })(MemoForm)
 
 export default MemoForm
