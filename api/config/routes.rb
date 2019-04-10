c641bc1a3e7b20f0e39a01a8e63377c3db1fa5e8
Suzuki Shogo <shogo807@gmail.com>
2019-04-10 20:56:15 +0900

    add auth

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'memos#index'
   resources :memos
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+  }
 end
